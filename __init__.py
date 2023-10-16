061da56bfe688c2ef5c30ef2fe81bbffa1522f41
PJ Kelly <40860586+kellypj24@users.noreply.github.com>
2023-10-16 12:39:04 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,25 @@
+import os
+
+from dagster import Definitions, InMemoryIOManager, IOManager
+from dagster_dbt import DbtCliResource
+from dagster_slack import SlackResource
+
+from cureatr.assets.dbt_assets import daily_dbt_assets_schedule, dw_dbt_assets
+
+
+resources = {
+    "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
+    "slack": SlackResource(token=os.getenv("SLACK_TOKEN")),
+}
+
+definitions = Definitions(
+    assets=[
+         dw_dbt_assets,
+    ],
+    jobs=[
+    ],
+    resources=resources,
+    schedules=[
+        daily_dbt_assets_schedule,
+    ],
+)
