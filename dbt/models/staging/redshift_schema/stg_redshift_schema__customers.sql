217253ccff4e8b684c0cda8dca036c1ed08e2aec
PJ Kelly <40860586+kellypj24@users.noreply.github.com>
2023-10-30 12:18:09 -0400

    Create stg_redshift_schema__customers.sql

---
+++ dbt/models/staging/redshift_schema/stg_redshift_schema__customers.sql
@@ -0,0 +1,19 @@
+with
+{{ import(source('redshift_schema', 'source_customers'), 'customers') }},
+
+filter_customers as (
+
+  select *
+  
+  from customers
+  where customers.id in (
+    customers.state in (
+        {% for state in var('us_states', []) -%}
+             '{{ state }}'
+             {%- if not loop.last %},{{ '\n' }}{% endif %}
+        {%- endfor %}
+    )
+
+  select *
+  from filter_customers
+ 
